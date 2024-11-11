681ec7957a905481221109fb4d50c3edad140961
Dat Doan <datdv1@smartosc.com>
2024-11-11 11:31:41 +0700

    Init App

---
+++ EnterpriseBankingApp/Sources/App/EnterpriseBankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnterpriseBankingAppApp.swift
+//  EnterpriseBankingApp
+//
+//  Created by doandat on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EnterpriseBankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
